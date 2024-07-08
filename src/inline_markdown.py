bb0a007a4a13832f5d376f5b2806459eeaf4c8a9
Tomma-pwd <babaski00@mail.ru>
2024-07-08 18:09:47 +0500

    add split_nodes_link/image

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -11,11 +11,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             if not len(split_text_list)%2 or len(split_text_list) < 3:
                 raise Exception(f"Odd amount of delimiter '{delimiter}' in text: amount = {text_node.text.count(delimiter)}")
             for i in range(len(split_text_list)):
-                if i%2:
-                    new_nodes.append(TextNode(split_text_list[i], text_type))
-                else:
-                    new_nodes.append(TextNode(split_text_list[i], TextNode.text_type_text))
-            break
+                if split_text_list[i] != '':
+                    if i%2:
+                        new_nodes.append(TextNode(split_text_list[i], text_type))
+                    else:
+                        new_nodes.append(TextNode(split_text_list[i], TextNode.text_type_text))
     return new_nodes
 
 def extract_markdown_images(text):
@@ -23,3 +23,46 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextNode.text_type_text:
+            new_nodes.append(node)
+            continue
+        image_tuple = extract_markdown_images(node.text)
+        if len(image_tuple) == 0:
+            new_nodes.append(node)
+        else:
+            for image in image_tuple:
+                split_nodes_text = node.text.split(f"![{image[0]}]({image[1]})")
+                if len(split_nodes_text) != 2:
+                    raise ValueError("Invalid markdown, image section not closed")
+                if split_nodes_text[0] != '':
+                    new_nodes.append(TextNode(split_nodes_text[0], TextNode.text_type_text))
+                new_nodes.append(TextNode(image[0], TextNode.text_type_image, url = image[1]))
+                if split_nodes_text[1] != '':
+                    new_nodes.append(TextNode(split_nodes_text[0], TextNode.text_type_text))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextNode.text_type_text:
+            new_nodes.append(node)
+            continue
+        link_tuple = extract_markdown_links(node.text)
+        if len(link_tuple) == 0:
+            new_nodes.append(node)
+        else:
+            for link in link_tuple:
+                split_nodes_text = node.text.split(f"![{link[0]}]({link[1]})")
+                if len(split_nodes_text) != 2:
+                    raise ValueError("Invalid markdown, link section not closed")
+                if split_nodes_text[0] != '':
+                    new_nodes.append(TextNode(split_nodes_text[0], TextNode.text_type_text))
+                new_nodes.append(TextNode(link[0], TextNode.text_type_image, url = link[1]))
+                if split_nodes_text[1] != '':
+                    new_nodes.append(TextNode(split_nodes_text[1], TextNode.text_type_text))
+    return new_nodes
