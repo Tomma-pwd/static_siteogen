32ea41855d57fa55d9016abeafe64ddc10ca1ea4
Tomma-pwd <babaski00@mail.ru>
2024-07-09 12:10:18 +0500

    add markdown_to_HTMLNodes

--- src/textnode.py
+++ src/textnode.py
@@ -5,14 +5,7 @@ text_type_italic = "italic"
 text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
-class TextNode:
-    text_type_text = "text"
-    text_type_bold = "bold"
-    text_type_italic = "italic"
-    text_type_code = "code"
-    text_type_link = "link"
-    text_type_image = "image"
-    
+class TextNode:    
     def __init__(self, text, text_type, url = None):
         self.text = text
         self.text_type = text_type
@@ -24,20 +17,20 @@ class TextNode:
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
 
-    def text_node_to_html_node(text_node):
-        if text_node is TextNode:
-            if text_node.text_type == TextNode.text_type_text:
-                return LeafNode(None, value = text_node.text)
-            if text_node.text_type == TextNode.text_type_bold:
-                return LeafNode(tag = "b", value = text_node.text)
-            if text_node.text_type == TextNode.text_type_italic:
-                return LeafNode(tag = "i", value = text_node.text)
-            if text_node.text_type == TextNode.text_type_code:
-                return LeafNode(tag = "code", value = text_node.text)
-            if text_node.text_type == TextNode.text_type_link:
-                return LeafNode(tag = "a", value = text_node.text, prop = {"href":text_node.url})
-            if text_node.text_type == TextNode.text_type_image:
-                return LeafNode(tag = "img", value = "", prop = {"src":text_node.url, "alt":text_node.text})
-            raise ValueError(f"Not supported text_type: {text_node.text_type}")
-        else:
-            raise TypeError("text_node is not of type TextNode")
+def text_node_to_html_node(text_node):
+    if text_node is TextNode:
+        if text_node.text_type == TextNode.text_type_text:
+            return LeafNode(None, value = text_node.text)
+        if text_node.text_type == TextNode.text_type_bold:
+            return LeafNode(tag = "b", value = text_node.text)
+        if text_node.text_type == TextNode.text_type_italic:
+            return LeafNode(tag = "i", value = text_node.text)
+        if text_node.text_type == TextNode.text_type_code:
+            return LeafNode(tag = "code", value = text_node.text)
+        if text_node.text_type == TextNode.text_type_link:
+            return LeafNode(tag = "a", value = text_node.text, prop = {"href":text_node.url})
+        if text_node.text_type == TextNode.text_type_image:
+            return LeafNode(tag = "img", value = "", prop = {"src":text_node.url, "alt":text_node.text})
+        raise ValueError(f"Not supported text_type: {text_node.text_type}")
+    else:
+        raise TypeError("text_node is not of type TextNode")
